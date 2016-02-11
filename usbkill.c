ac70ca0e45f29ecee1c9070ca5eb2da1239162dd
Nate Brune <natebrune@gmail.com>
2016-02-11 15:55:56 -0500

    Update usbkill.c

--- usbkill.c
+++ usbkill.c
@@ -32,7 +32,14 @@ bool stop = false;
 
 static struct task_struct *thread1;
 
-
+static void kernel_shutdown_prepare(enum system_states state)
+{
+	blocking_notifier_call_chain(&reboot_notifier_list,
+		(state == SYSTEM_HALT) ? SYS_HALT : SYS_POWER_OFF, NULL);
+	system_state = state;
+	usermodehelper_disable();
+	device_shutdown();
+}
 
 int guardian(void){
 	while(true)
